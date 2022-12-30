39796eac782d3126c4b87ab356a6c7d9dfc8d96e
Aviral1344 <aviralgupta1344@gmail.com>
2022-12-30 21:17:27 +0400

    just another day

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {initializeApp} from 'firebase/app';
+
 
 const forms= document.querySelector(".forms"),
     pwShowHide= document.querySelectorAll(".eye-icon"),
@@ -23,6 +23,7 @@ pwShowHide.forEach(eyeIcon =>
         })
     })
 
+
 links.forEach(link =>
     {
         link.addEventListener("click", e =>
@@ -32,3 +33,93 @@ links.forEach(link =>
         })
     })
 
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB2MmNbTTTHEhuJ0DMMjMFO8lGpelwOB_4",
+    authDomain: "beanjam-7a98e.firebaseapp.com",
+    databaseURL: "https://beanjam-7a98e-default-rtdb.firebaseio.com",
+    projectId: "beanjam-7a98e",
+    storageBucket: "beanjam-7a98e.appspot.com",
+     messagingSenderId: "361212926453",
+    appId: "1:361212926453:web:a0c057737022028f8228bc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
+    
+//Signup function
+function signup(){
+    email= document.getElementById('emailS').value;
+    password= document.getElementById('pwdS').value;
+    Uname= document.getElementById('name').value;
+
+    if(validate_email(email)==flase){
+        alert("Invalid Email Id")
+        return
+    }
+
+    if(validate_password(password)==false){
+            alert("Pasword should be 6 charecters or longer")
+            return
+        }
+
+    if(validate_field(email)==flase || validate_field(password)==false){
+        alert("One or more fields are empty")
+        return
+    }
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function(){
+
+        var user= auth.currentUser
+        var database_ref = database.ref()
+
+        var user_data= {
+            email: email,
+            name: Uname,
+            last_login: Date.now()
+        }
+            database_ref.child('user/' + user.uid).set(user_data)
+            alert('User created')
+
+        })
+
+    .catch(function(error){
+        var error_code= error.code
+        var error_message= error.message
+        alert(error_message)
+    })
+}
+
+function validate_email(email){
+    expression = /^[^@]+@\w+(\.\w+)+\w$/.test(str);
+    if (expression.test(email)== true){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+function validate_password(password){
+    if(password<8){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function validate_field(field){
+    if(field == null){
+        return false
+    }
+
+    if(field.length<=0){
+        return false
+    }
+    else{
+        return true
+    }
+}
